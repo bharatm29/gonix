f4d63d63ad3969a0db845d50d7c20092510ab6f8
bharatm29 <bharatmhes@gmail.com>
2024-03-03 13:27:43 +0530

    long flag done onto size format and date modified

--- ls/ls.go
+++ ls/ls.go
@@ -8,8 +8,17 @@ import (
 	"path/filepath"
 )
 
+type File struct {
+	name  string
+	perms string
+	size  float64
+	isDir bool
+}
+
 func main() {
 	hiddenFlag := flag.Bool("a", false, "Show hidden directories and files")
+	longFlag := flag.Bool("l", false, "Show verbose directory and file info")
+
 	flag.Parse()
 
 	if len(flag.Args()) < 1 {
@@ -17,19 +26,25 @@ func main() {
 		os.Exit(1)
 	}
 
-	filenames := []string{}
+	dirs := []File{}
+	files := []File{}
 
 	filepath.WalkDir(flag.Arg(0), func(path string, items os.DirEntry, err error) error {
-		/*TODO: Implement -l
-		  info, err := items.Info()
-		*/
-
 		if err != nil {
 			fmt.Printf("[ERROR] Could not recursive down the directory: %s", err)
 			return filepath.SkipAll
 		}
+
+		info, err := items.Info()
+		if err != nil {
+			fmt.Printf("[ERROR] Could not get directory or file into: %s", err)
+			return filepath.SkipAll
+		}
+
 		if items.IsDir() {
 			dirName := items.Name()
+
+			var coloredName string
 
 			if path == flag.Arg(0) {
 				return nil
@@ -37,13 +52,21 @@ func main() {
 
 			if len(dirName) > 0 && dirName[0] == '.' {
 				if *hiddenFlag {
-					fmt.Print(color.Colorize(color.GRAY, fmt.Sprintf("%s/ ", dirName)))
+					coloredName = (color.Colorize(color.GRAY, fmt.Sprintf("%s/ ", dirName)))
 				} else {
 					return filepath.SkipDir
 				}
 			} else {
-				fmt.Print(color.Colorize(color.RED, fmt.Sprintf("%s/ ", dirName)))
+				coloredName = (color.Colorize(color.RED, fmt.Sprintf("%s/ ", dirName)))
 			}
+
+			dir := File{
+				name:  coloredName,
+				perms: info.Mode().String(),
+				size:  float64(info.Size()) / 1000,
+				isDir: true,
+			}
+			dirs = append(dirs, dir)
 
 			return filepath.SkipDir
 		} else {
@@ -61,14 +84,34 @@ func main() {
 				coloredName = color.Colorize(color.BLUE, fmt.Sprintf("%s ", fileName))
 			}
 
-			filenames = append(filenames, coloredName)
+			file := File{
+				name:  coloredName,
+				perms: info.Mode().String(),
+				size:  float64(info.Size()) / 1000,
+				isDir: false,
+			}
+			files = append(files, file)
 			return nil
 		}
 	})
 
-	for _, filename := range filenames {
-		fmt.Print(filename + " ")
+	for _, dir := range dirs {
+		if *longFlag {
+			fmt.Printf("%s %.1fk %s\n", dir.perms, dir.size, dir.name)
+		} else {
+			fmt.Printf("%s ", dir.name)
+		}
 	}
 
-	fmt.Printf("\n")
+	for _, file := range files {
+		if *longFlag {
+			fmt.Printf("%s %.1fk %s\n", file.perms, file.size, file.name)
+		} else {
+			fmt.Printf("%s ", file.name)
+		}
+	}
+
+	if !(*longFlag) {
+		fmt.Print("\n")
+	}
 }
